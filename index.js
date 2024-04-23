89cace331c9116e9d67e8ca349e4abd13a98ea90
Daniela <20220434@ricaldone.edu.sv>
2024-04-23 10:03:47 -0600

    ultimos cambios

---
+++ index.js
@@ -0,0 +1,8 @@
+// arreglo de 10 cosas favoritas 
+var artistas = ["Justin Bieber", "Chase Atlantic", "Joao Felix", "Real Madrid", "Artic Monkeys", "La Casa de Papel", "Ropa", "Fresas", ""];
+
+
+// recorre arreglo usando forEach
+artistas.forEach(function(elemento) {
+   console.log(elemento); // imprimirlo en consola 
+});
