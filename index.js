7f2e290c364e43c00b0414806d3e1e506010d7ff
AxelGarciaR <20220127@ricaldone.edu.sv>
2024-04-23 15:32:45 -0600

    practica de arreglos javascript

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // arreglo de 10 cosas favoritas 
-var artistas = ["Justin Bieber", "Chase Atlantic", "Joao Felix", "Real Madrid", "Artic Monkeys", "La Casa de Papel", "Ropa", "Fresas", ""];
+var artistas = ["Justin Bieber", "Chase Atlantic", "Joao Felix", "Real Madrid", "Artic Monkeys", "La Casa de Papel", "Carro", "Fresas", "Perros", "Cielo"];
 
 
 // recorre arreglo usando forEach
